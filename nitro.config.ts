f39b65092754d7aceaac98ed54907bfa8fe96508
varna <paul.varna@gmail.com>
2023-03-17 15:53:53 +0200

    1 file modified

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,10 +1,10 @@
 import { defineNitroConfig } from 'nitropack';
 
 export default defineNitroConfig({
-  routeRules: {
-    '/api/**': {
-      cors: true,
-      headers: { 'access-control-allow-methods': 'GET,POST,OPTIONS' },
-    },
-  },
+  // routeRules: {
+  //   '/api/**': {
+  //     cors: true,
+  //     headers: { 'access-control-allow-methods': 'GET,POST,OPTIONS' },
+  //   },
+  // },
 });
