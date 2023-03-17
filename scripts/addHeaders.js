0ff6a272f15c22e5f0228308bd669e89caedf891
varna <paul.varna@gmail.com>
2023-03-17 16:27:32 +0200

    1 file modified

--- scripts/addHeaders.js
+++ scripts/addHeaders.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 
+// Access-Control-Allow-Method: POST, GET, OPTIONS
 const content = `/*
-  Access-Control-Allow-Method: POST, GET, OPTIONS`;
+  Access-Control-Allow-Origin: *
+  Access-Control-Allow-Methods: *
+  Access-Control-Allow-Headers: *
+  Access-Control-Max-Age: 0`;
 
 console.log('Hotfix: _headers');
 fs.writeFile('./dist/_headers', content, (err) => {
